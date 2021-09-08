48b73b475ed061ab334579a3927a60a1a08c0f4d
Kapil Arora <infykapil.arora@gmail.com>
2021-09-08 22:31:33 +0530

    Update Linkedlst.py

--- Linkedlst.py
+++ Linkedlst.py
@@ -15,6 +15,34 @@ class SLinkedList:
          print (printval.dataval)
          printval = printval.nextval
 
+    def AtBegining(self,newdata):
+      NewNode = Node(newdata)
+# Update the new nodes next val to existing node
+      NewNode.nextval = self.headval
+      self.headval = NewNode
+
+    def AtEnd(self,newdata):
+        NewNode = Node(newdata)
+# Update the existing last node to point to the new node added in the end
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        laste = self.headval
+        while (laste.nextval):
+            laste = laste.nextval
+        laste.nextval=NewNode
+
+    def InBetween(self, middlenode, newdata):
+        if middlenode is None:
+            print ("the mentioned node is absent")
+            return
+        NewNode = Node(newdata)
+        NewNode.nextval = middlenode.nextval
+        middlenode.nextval = NewNode
+
+
+    
+
 list1 = SLinkedList()
 list1.headval = Node("Mon")
 e2 = Node("Tue")
@@ -30,3 +58,17 @@ e2.nextval = e3
 
 list1.listprint()
 
+list1.AtBegining("Sun")
+print('\n',"*******************************", '\n', "printing new row" , '\n', "******************************")
+list1.listprint()
+
+list1.AtEnd("Thu")
+print('\n',"*******************************", '\n', "printing new row" , '\n', "******************************")
+list1.listprint()
+
+print('\n',"*******************************", '\n', "printing new row" , '\n', "******************************")
+
+
+list1.InBetween(list1.headval.nextval,"Fri")
+
+list1.listprint()
