89049d26f361bcacc6282083bf748329e00953e2
Kapil Arora <infykapil.arora@gmail.com>
2021-09-13 23:13:20 +0530

    Update CircularLLst.py

--- Data Structures/LinkedList/CircularLLst.py
+++ Data Structures/LinkedList/CircularLLst.py
@@ -11,6 +11,7 @@ class CLinkedList:
 
     def insert(self, dataval):
         var = Node(dataval)
+        var.next = self.head
         #print("Curr Value is {} and next value is {}".format(var.datav,var.next))
         if self.head is None:
             self.head = var
@@ -19,23 +20,31 @@ class CLinkedList:
             start = self.head
             while start is not None:
                 if start.next is None:
-                    start.next = dataval                    
+                    start.next = var 
                     return
+                elif start.next == self.head:
+                     start.next = var
+                     return   
                 else:
                     print("Next value is {}".format(start.next))
                     start = start.next
         return
+
 
     def printlist(self):
         printval = self.head
         while printval is not None:
             print (printval.datav)
             printval = printval.next
+            if printval == self.head:
+                break
+           
 
 
 list = CLinkedList()
 list.insert(1)
 list.insert(2)
+list.insert(3)
 list.printlist()
 
 
